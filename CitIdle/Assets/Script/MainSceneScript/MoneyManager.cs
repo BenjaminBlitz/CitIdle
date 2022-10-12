c639c7e4f1021033c0d9583c2a04d40edef713c1
HNConan <hadrien.conan@edu.esiee.fr>
2022-10-12 16:23:20 +0200

    Up

--- CitIdle/Assets/Script/MainSceneScript/MoneyManager.cs
+++ CitIdle/Assets/Script/MainSceneScript/MoneyManager.cs
@@ -9,11 +9,32 @@ public class MoneyManager : MonoBehaviour
     public static float InternalMoney;
     public float MoneyCount;
     public GameObject DisplayMoney;
+    public float InternalIncrease;
+    public float MoneyIncrease;
+    public bool CreatingMoney;
 
 
     private void Update()
     {
+
         MoneyCount = InternalMoney;
-       DisplayMoney.GetComponent<TMP_Text>().text = " $ = " + MoneyCount;
+        if (CreatingMoney == false)
+        {
+            CreatingMoney = true;
+           // StartCoroutine(CreateMoney());
+
+        }
+        MoneyCount += (float)(PopulationManager.PopulationCount * 0.000001);
+        DisplayMoney.GetComponent<TMP_Text>().text = " $ = " + MoneyCount;
     }
+
+
+
+
+   /* IEnumerator CreateMoney()
+    {
+       /* MoneyCount += (float) InternalIncrease * PopulationManager.PopulationCount*0.1);
+        yield return new WaitForSeconds(1);
+        CreatingCrystal = false;
+    }*/
 }
