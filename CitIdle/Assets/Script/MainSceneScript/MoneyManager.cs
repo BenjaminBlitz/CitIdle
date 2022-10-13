7de01d0e292ea1b53f1d0868900502ae1aad2811
HNConan <hadrien.conan@edu.esiee.fr>
2022-10-13 10:43:41 +0200

    Revert "Up"
    This reverts commit dad9e62290831f69cb04a3397794a1d15ede5c8a.

--- CitIdle/Assets/Script/MainSceneScript/MoneyManager.cs
+++ CitIdle/Assets/Script/MainSceneScript/MoneyManager.cs
@@ -9,29 +9,11 @@ public class MoneyManager : MonoBehaviour
     public static float InternalMoney;
     public float MoneyCount;
     public GameObject DisplayMoney;
-    public float InternalIncrease;
-    public float MoneyIncrease;
-    public bool CreatingMoney;
 
 
     private void Update()
     {
-        InternalMoney += (float)(PopulationManager.PopulationCount * 0.0000001);
         MoneyCount = InternalMoney;
-        if ( MoneyCount<0)
-        {
-            MoneyCount = 0;
-        }
-        DisplayMoney.GetComponent<TMP_Text>().text = " $ = " + InternalMoney;
-
+       DisplayMoney.GetComponent<TMP_Text>().text = " $ = " + MoneyCount;
     }
-
-
-
-
-
-
-
-
- 
 }
