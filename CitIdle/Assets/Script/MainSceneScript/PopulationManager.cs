12d46cb3aa40536f5d862ca02f9eb2f3077d95d7
BenjaminBlitz <benjamin.blitz@edu.esiee.fr>
2022-10-13 10:22:05 +0200

    Revert "test"
    This reverts commit 88df4aa132bf942b3d3991bcc8504242dbac99ff, reversing
    changes made to e30ab8d540bcee7cb12ad3b8a4d7a6e0fab4f605.

--- CitIdle/Assets/Script/MainSceneScript/PopulationManager.cs
+++ CitIdle/Assets/Script/MainSceneScript/PopulationManager.cs
@@ -11,59 +11,11 @@ public class PopulationManager : MonoBehaviour
     public static float PopulationCount;
     public float InternalPopulation;
     public GameObject DisplayPop;
-    public float nextActionTime = 0;
-    public float period = 0.3f;
-
-    private void Start()
-    {
-        switch (LevelManager.statLevelMairie)
-        {
-            case 1:
-                InternalPopulation = 1000;
-                PopulationCount = InternalPopulation;
-
-                break;
-            case 2:
-                InternalPopulation = 10000;
-                
-                break;
-            case 3:
-                InternalPopulation = 100000;
-                break;
-            case 4:
-                InternalPopulation = 1000000;
-                break;
 
 
-        }
-    }
     private void Update()
     {
-
-
-
-        if (PolutionManager.statHighPollution)
-        {
-             if ( Time.time > nextActionTime)
-            {
-            nextActionTime += period;
-                PopulationCount--;
-
-               }
-
-
-           
-
-        }
-        if(!PolutionManager.statHighPollution && PopulationCount< InternalPopulation*10)
-        {
-            if (Time.time > nextActionTime)
-            {
-                nextActionTime += period;
-                PopulationCount++;
-            }
-        }
-
+        PopulationCount = InternalPopulation;
         DisplayPop.GetComponent<TMP_Text>().text = " Pop = " + PopulationCount;
     }
 }
