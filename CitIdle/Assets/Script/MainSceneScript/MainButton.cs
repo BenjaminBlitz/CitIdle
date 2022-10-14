381cb4a3213c04ae01766801e6be0afd5dc87094
HNConan <hadrien.conan@edu.esiee.fr>
2022-10-14 12:51:21 +0200

    final

--- CitIdle/Assets/Script/MainSceneScript/MainButton.cs
+++ CitIdle/Assets/Script/MainSceneScript/MainButton.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MainButton : MonoBehaviour
 {
-    public void clickOnButton()
+    public static void clickOnButton()
     {
         MoneyManager.InternalMoney ++;
     }
